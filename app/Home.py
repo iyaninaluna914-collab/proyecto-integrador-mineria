16df077cd73e50dedd851334211f2f8e06890b77
iyaninaluna914-collab <iyaninaluna914@gmail.com>
2026-07-01 17:55:08 -0300

    Add files via upload

---
+++ app/Home.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(page_title="Proyecto Minería de Datos", layout="wide")
+
+st.title("📊 Proyecto Integrador - Minería de Datos 1")
+
+st.write("""
+Este proyecto consiste en el análisis exploratorio de datos de usuarios de una plataforma de streaming.
+
+Se aplicaron técnicas de limpieza, análisis exploratorio y reducción de dimensionalidad (PCA).
+""")
+
+st.subheader("👥 Integrantes")
+st.write("- Luna Yanina")
+st.write("- Luna Karina ")
+st.subheader("🎓 Comisión")
+st.write("Turno Mañana")
+
+st.subheader("📅 Fecha")
+st.write("Julio 2026")
+st.markdown("""
+🔗 **Repositorio del proyecto:**  
+https://github.com/iyaninaluna914-collab/proyecto-integrador-mineria
+""")
---
+++ app/pages/01_Dataset.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.title("📁 Dataset")
+
+df = pd.read_csv("../data/processed/dataset_limpio.csv")
+
+st.subheader("Vista previa del dataset")
+st.dataframe(df.head())
+
+st.subheader("Información general")
+st.write(df.info())
+
+st.subheader("Estadísticas descriptivas")
+st.write(df.describe())
