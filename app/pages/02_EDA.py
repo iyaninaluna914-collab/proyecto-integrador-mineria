85302eb66b46392b18608884123a57fcfad293c8
iyaninaluna914-collab <iyaninaluna914@gmail.com>
2026-07-01 19:59:21 -0300

    Update 02_EDA.py

--- app/pages/02_EDA.py
+++ app/pages/02_EDA.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("../data/processed/dataset_limpio.csv")
+df = pd.read_csv("data/processed/dataset_limpio.csv")
 
 st.title("📊 EDA")
 
