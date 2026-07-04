ea3889adeb332cc24a8245bb26541f2caac37325
iyaninaluna914-collab <iyaninaluna914@gmail.com>
2026-07-04 15:53:32 -0300

    Delete app directory

--- app/pages/02_EDA.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# =========================
-# CARGA DE DATOS LIMPIOS
-# =========================
-
-def load_data():
-    df = pd.read_csv("data/processed/dataset_limpio.csv")
-    return df
